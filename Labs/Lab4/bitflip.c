3df47a3aa4e2b19c96106891a539e3ef75f942ee
lustered <smellslikelavenders@gmail.com>
2021-05-27 23:21:38 -0400

    Fixed bit flip

--- Labs/Lab4/bitflip.c
+++ Labs/Lab4/bitflip.c
@@ -9,25 +9,33 @@
  * Usage: bitflip
  */
 
+#include <stdint.h>
 #include <stdio.h>
 
 int main() {
 
-  unsigned int userNum = 0;
-  int flip = 0;
+  uint32_t userNum = 0;
+  int nth = 0;
 
   do {
-    printf("Please enter a number between 1-100: ");
+    printf("Please enter a number between 1-1000: ");
     scanf("%u", &userNum);
 
   } while (userNum < 1 || userNum > 1000);
 
   do {
     printf("Please enter the bit to flip between 0 and 31: ");
-    scanf("%d", &flip);
-  } while (flip < 0 || flip > 31);
+    scanf("%d", &nth);
+  } while (nth < 0 || nth > 31);
 
-  printf("New value: %u\n", userNum ^ flip);
+  // Breakdown:
+  // (1 << nth position {2} ) 32 bit: 000001 << 2 = 000100 (4)
+  //    001010 (10) userNum
+  // xor
+  //    000100 (4) 1 << nth
+  //    ------
+  //    001110 (14)
+  printf("New value: %u\n", userNum ^ (1 << nth));
 
   return 0;
 }
