e09753247948571ba4f48417cd63a5400e040ccf
lustered <smellslikelavenders@gmail.com>
2021-07-08 23:16:34 -0400

    Closing fds properly, misc

--- Labs/Lab5/counter.c
+++ Labs/Lab5/counter.c
@@ -9,34 +9,35 @@
  * unix command equivalent: sort counter.c | grep main | wc
  */
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <sys/wait.h>
 #include <unistd.h>
 
 #define FD_READ 0
 #define FD_WRITE 1
 
 int main() {
-  int sortPipe[2], grepPipe[2]; // Declare two buffers to use as fd.
-  pid_t pid;                    // id for child processes.
+  int sortPipe[2], grepPipe[2]; // Declare two buffers to use as fd
+  pid_t pid;                    // ID for child processes
 
-  // List of commands to run.
+  // List of commands to run
   static char *commands[3][3] = {{"/bin/sort", "sort", "counter.c"},
                                  {"/usr/bin/grep", "grep", "main"},
-                                 {"/usr/bin/wc", "wc", ""}};
+                                 {"/usr/bin/wc", "wc", NULL}};
 
-  pipe(sortPipe); // Create the first pipe.
+  pipe(sortPipe); // Create the first pipe
 
   // Get stdout of sort
   if ((pid = fork()) == 0) {
+    // Don't need to read
+    close(sortPipe[FD_READ]);
+
     // Only need to write to the pipe.
     dup2(sortPipe[FD_WRITE], FD_WRITE);
 
     // Execute sort counter.c
     execlp(commands[0][0], commands[0][1], commands[0][2], NULL);
 
-    exit(0); // Close process.
+    exit(1); // Close process.
   }
 
   // Don't need to write to this pipe anymore.
@@ -47,6 +48,9 @@ int main() {
 
   // Get stdout from sort and grep main
   if ((pid = fork()) == 0) {
+    // Don't need to read 
+    close(grepPipe[FD_READ]);
+
     // Read from the sort pipe
     dup2(sortPipe[FD_READ], FD_READ);
 
@@ -56,10 +60,10 @@ int main() {
     // Execute grep main
     execlp(commands[1][0], commands[1][1], commands[1][2], NULL);
 
-    exit(0); // Close process.
+    exit(1); // Close process
   }
 
-  // Don't need to write to these pipes anymore.
+  // Don't need to use these pipes anymore
   close(sortPipe[FD_READ]);
   close(grepPipe[FD_WRITE]);
 
@@ -69,10 +73,13 @@ int main() {
     dup2(grepPipe[FD_READ], FD_READ);
 
     // execute wc {grep's output}
-    execlp(commands[2][0], commands[2][1], NULL);
+    execlp(commands[2][0], commands[2][1], commands[2][2], NULL);
 
-    exit(0); // Close process.
+    exit(1); // Close process.
   }
+
+  // We are done reading contents
+  close(grepPipe[FD_READ]);
 
   return 0;
 }
